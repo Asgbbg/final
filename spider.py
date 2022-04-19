03d7a4417299cbea618e694f1795c3a973b146a7
asgbbg <asgbbg@hotmail.com>
2022-04-19 22:35:27 +0800

    根据classname获取相关文本，返回一个list，可根据list遍历出该用户所有内容，tags可直接获取该游戏的所有tags

--- spider.py
+++ spider.py
@@ -1,10 +1,11 @@
 from time import sleep
-from selenium import  webdriver
-browser=webdriver.Chrome()
-from  selenium.webdriver.common.by import By
-import  requests
-from  lxml import  etree
-url='https://www.taptap.com/user/4393096/reviews'
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+from selenium.webdriver.common.by import By
+
+url = 'https://www.taptap.com/user/4393096/reviews'
 # url='https://www.baidu.com'
 # header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}
 # html=requests.get(url,headers=header).text
@@ -13,11 +14,16 @@ url='https://www.taptap.com/user/4393096/reviews'
 # content=etree_html.xpath('//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[7]/div/div[9]/div/div/div[1]/div/div[2]/div[3]/a/div/div/div/p')
 browser.get(url)
 # text=browser.page_source
-sleep(5.0)
-title=browser.find_element(by=By.XPATH,value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/a/div/p/span').text
-text=browser.find_element(by=By.XPATH,value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[3]/a/div/div/div/p').text
-tag1=browser.find_element(by=By.XPATH,value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]//a/div').text
-# tag2=browser.find_element(by=By.XPATH,value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/a[2]/div')
+sleep(10.0)
+title = browser.find_element(by=By.XPATH,
+                             value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/a/div/p/span').text
+text = browser.find_element(by=By.XPATH,
+                            value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[3]/a/div/div/div/p').text
+tags = browser.find_elements(by=By.XPATH,
+                             value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]')[
+    0].text
+texts = browser.find_elements(by=By.CLASS_NAME, value='text-box__content')[1].text
+# tag2=browser.find_element(by=By.XPATH,value='//*[@id="tap"]/div/main/div[2]/div/div/div[3]/div[3]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/a[2]/div').text
 
-print(title,text,tag1)
+print(title, text, tags, texts)
 # print(content)
